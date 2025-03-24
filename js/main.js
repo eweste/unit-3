4f06c5c42377400f6312a46ee1e77ba7bd3cf32c
Ethan Westerkamp <ewesterkamp@wisc.edu>
2025-03-24 10:31:12 -0400

    Activity9and10

--- js/main.js
+++ js/main.js
@@ -1,169 +1,242 @@
-// Add all scripts to the JS folder
+//Ethan Westerkamp
 
+(function(){
+
+var attrArray = ["protect_area", "real_gdp", "uaa", "renew_nrg", "protect_area", "CO2"];
+var expressed = attrArray[0];
 //execute script when window is loaded
-window.onload = function(){
 
-    var w = 900, h = 500;
+window.onload = setMap();
 
-     //Example 1.2 line 1...container block
-      //Example 1.5 line 1...container block
-    var container = d3.select("body") //get the <body> element from the DOM
-    .append("svg") //put a new svg in the body
-    .attr("width", w) //assign the width
-    .attr("height", h) //assign the height
-    .attr("class", "container") //assign a class name
-    .style("background-color", "rgba(0,0,0,0.2)"); //svg background color
+    //set up choropleth map
+function setMap() {
 
-//innerRect block
-var innerRect = container.append("rect")
-        .datum(400) //a single value is a DATUM
-        .attr("width", function(d){ //rectangle width
-            return d * 2; //400 * 2 = 800
-        })
-        .attr("height", function(d){ //rectangle height
-            return d; //400
-        })
-        .attr("class", "innerRect") //class name
-        .attr("x", 50) //position from left on the x (horizontal) axis
-        .attr("y", 50) //position from top on the y (vertical) axis
-        .style("fill", "#FFFFFF"); //fill color
-console.log(innerRect);
+    //map frame dimensions
+    var width = window.innerWidth *0.5,
+    height = 600;    
 
-//Example 2.3 line 1
-var dataArray = [10, 20, 30, 40, 50];
+    var map = d3.select("body")
+    .append("svg")
+    .attr("class", "map")
+    .attr("width", width)
+    .attr("height", height);
 
-var cityPop = [
-    { 
-        city: 'Madison',
-        population: 233209
-    },
-    {
-        city: 'Milwaukee',
-        population: 594833
-    },
-    {
-        city: 'Green Bay',
-        population: 104057
-    },
-    {
-        city: 'Superior',
-        population: 27244
+    var projection = d3.geoAlbers()
+        .center([10, 46.2])
+        .rotate([-6, -7])
+        .parallels([45, 60])
+        .scale(900)
+        .translate([width / 2, height / 2]);
+
+    var path = d3.geoPath()
+        .projection(projection);
+
+    //use Promise.all to parallelize asynchronous data loading
+    var promises = [];
+
+    promises.push(d3.csv("data/aggregatedata.csv")); //load attributes from csv    
+    promises.push(d3.json("data/eu_region.topojson")); //load background spatial data    
+    promises.push(d3.json("data/EU_area_countries.topojson")); //load choropleth spatial data 
+
+    Promise.all(promises).then(callback);
+
+    function callback(data) {
+
+        
+
+        var csvData = data[0],
+            europe_region = data[1],
+            eu_states = data[2];
+
+        setGraticulte(map, path);
+
+        var region = topojson.feature(europe_region, europe_region.objects.eu_region);
+        var eu_countries = topojson.feature(eu_states, eu_states.objects.EU_area_countries).features;
+
+        
+
+        
+
+
+        var map_region = map.append("path")
+            .datum(region)
+            .attr("class", "regions")
+            .attr("d", path);    
+
+        eu_countries = joinData(eu_countries, csvData);
+        
+        //create the color scale
+        var colorScale = makeColorScale(csvData);
+
+        setEnumerationUnits(eu_countries, map, path, colorScale);
+        
+        setChart(csvData, colorScale);
+        console.log(csvData);
+        console.log(eu_countries);
     }
-];
+    };
 
-var x = d3.scaleLinear() //create the scale
-        .range([90, 810]) //output min and max
-        .domain([0, 3.3]); //input min and max
+function makeColorScale(data){
+    var colorClasses = [
+        "#ffffcc",
+        "#c2e699",
+        "#78c679",
+        "#31a354",
+        "#006837"
+    ];
 
+    //create color scale generator
+    var colorScale = d3.scaleQuantile()
+        .range(colorClasses);
 
+    //build two-value array of minimum and maximum expressed attribute values
+    var minmax = [
+        d3.min(data, function(d) { return parseFloat(d[expressed]); }),
+        d3.max(data, function(d) { return parseFloat(d[expressed]); })
+    ];
+    //assign two-value array as scale domain
+    colorScale.domain(minmax);
 
-var minPop = d3.min(cityPop, function(d){
-        return d.population;
-    });
-
-    //find the maximum value of the array
-    var maxPop = d3.max(cityPop, function(d){
-        return d.population;
-    });
-
-    //scale for circles center y coordinate
-    var y = d3.scaleLinear()
-        .range([450, 50]) //was 440, 95
-        .domain([0, 700000]); //was minPop, maxPop
-
-
-var color = d3.scaleLinear()
-.range([
-    "#FDBE85",
-    "#D94701"
-])
-.domain([
-    minPop, 
-    maxPop
-]);
-
-var yAxis = d3.axisLeft(y);
-var axis = container.append("g")
-    .attr("class", "axis")
-    .attr("transform", "translate(50, 0)")
-    .call(yAxis);
-
-var title = container.append("text")
-.attr("class", "title")
-.attr("text-anchor", "middle")
-.attr("x", 450)
-.attr("y", 30)
-.text("City Populations");
-
-var labels = container.selectAll(".labels")
-        .data(cityPop)
-        .enter()
-        .append("text")
-        .attr("class", "labels")
-        .attr("text-anchor", "left")
-        .attr("y", function(d){
-            //vertical position centered on each circle
-            return y(d.population);
-        });
-
-    //first line of label
-    var nameLine = labels.append("tspan")
-        .attr("class", "nameLine")
-        .attr("x", function(d,i){
-            //horizontal position to the right of each circle
-            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-        })
-        .text(function(d){
-            return d.city;
-        });
-
-    //second line of label
-    var format = d3.format(",");
-
-    //Example 3.16 line 1...second line of label
-    var popLine = labels.append("tspan")
-        .attr("class", "popLine")
-        .attr("x", function(d,i){
-            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-        })
-        .attr("dy", "15") //vertical offset
-        .text(function(d){
-            return "Pop. " + format(d.population); //use format generator to format numbers
-        });
-    
-
-var circles = container.selectAll(".circles") //create an empty selection
-.data(cityPop) //here we feed in an array
-.enter() //one of the great mysteries of the universe
-.append("circle") //inspect the HTML--holy crap, there's some circles there
-.attr("class", "circles")
-.attr("id", function(d){
-    return d.city;
-})
-
-
-
-
-
-
-.attr("r", function(d){
-    //calculate the radius based on population value as circle area
-    var area = d.population * 0.01;
-    return Math.sqrt(area/Math.PI);
-})
-.attr("cx", function(d, i){
-    //use the scale generator with the index to place each circle horizontally
-    return x(i);
-})
-.attr("cy", function(d){
-    return y(d.population);
-})
-.style("fill", function(d, i){ //add a fill based on the color scale generator
-    return color(d.population);
-})
-.style("stroke", "#000"); //black circle stroke
-
-console.log(x)
+    return colorScale;
 };
+
+
+
+
+function setGraticulte(map, path) {
+    var graticule = d3.geoGraticule()
+            .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
+
+        var gratBackground = map.append("path")
+            .datum(graticule.outline()) //bind graticule background
+            .attr("class", "gratBackground") //assign class for styling
+            .attr("d", path) //project graticule
+        
+        var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
+            .data(graticule.lines()) //bind graticule lines to each element to be created
+            .enter() //create an element for each datum
+            .append("path") //append each element to the svg as a path element
+            .attr("class", "gratLines") //assign class for styling
+            .attr("d", path); //project graticule lines
+};
+
+function joinData(eu_countries, csvData) {
+    for (var i=0; i<csvData.length; i++){
+        var csvRegion = csvData[i]; //the current region
+        var csvKey = csvRegion.ABBREV; //the CSV primary key
+
+        //loop through geojson regions to find correct region
+        for (var a=0; a<eu_countries.length; a++){
+
+            var geojsonProps = eu_countries[a].properties; //the current region geojson properties
+            var geojsonKey = geojsonProps.Abbrev; //the geojson primary key
+
+            //where primary keys match, transfer csv data to geojson properties object
+            if (geojsonKey == csvKey){
+
+                //assign all attributes and values
+                attrArray.forEach(function(attr){
+                    var val = parseFloat(csvRegion[attr]); //get csv attribute value
+                    geojsonProps[attr] = val; //assign attribute and value to geojson properties
+                });
+            };
+        };
+    };
+    return eu_countries
+};
+
+function setEnumerationUnits(eu_countries, map, path, colorScale){
+    var specific_countries = map.selectAll()
+            .data(eu_countries)
+            .enter()
+            .append("path")
+            .attr("class", function(d){
+                return "ADMIN " + d.properties.ADMIN
+            })
+            .attr("d", path)
+            .style("fill", function(d){
+                var value = d.properties[expressed];
+                if(value) {
+                return colorScale(d.properties[expressed]);
+                } else {
+                    return "#ccc";
+                }
+            })
+};
+
+function setChart(csvData, colorScale){
+    //chart frame dimensions
+    var chartWidth = window.innerWidth * 0.425,
+        chartHeight = 600;
+
+    //create a second svg element to hold the bar chart
+    var chart = d3.select("body")
+        .append("svg")
+        .attr("width", chartWidth)
+        .attr("height", chartHeight)
+        .attr("class", "chart");
+
+    var yScale = d3.scaleLinear()
+    .range([0, chartHeight])
+    .domain([0, 105]);
+
+    var bars = chart.selectAll(".bars")
+        .data(csvData)
+        .enter()
+        .append("rect")
+        .sort(function(a, b){
+            return a[expressed]-b[expressed]
+        })
+        .attr("class", function(d){
+            return "bars " + d.renew_nrg;
+        })
+        .attr("width", chartWidth / csvData.length - 1)
+        .attr("x", function(d, i){
+            return i * (chartWidth / csvData.length);
+        })
+        .attr("height", function(d){
+            return yScale(parseFloat(d[expressed]));
+        })
+        .attr("y", function(d){
+            return chartHeight - yScale(parseFloat(d[expressed]));
+        })
+        .style("fill", function(d){
+            return colorScale(d[expressed]);
+        });
+
+    var numbers = chart.selectAll(".numbers")
+    .data(csvData)
+    .enter()
+    .append("text")
+    .sort(function(a, b){
+        return a[expressed]-b[expressed]
+    })
+    .attr("class", function(d){
+        return "numbers " + d.renew_nrg;
+    })
+    .attr("text-anchor", "middle")
+    .attr("x", function(d, i){
+        var fraction = chartWidth / csvData.length;
+        return i * fraction + (fraction - 1) / 2;
+    })
+    .attr("y", function(d){
+        return chartHeight - yScale(parseFloat(d[expressed])) + 15;
+    })
+    .text(function(d){
+        return d[expressed];
+    });
+
+    var chartTitle = chart.append("text")
+    .attr("x", 20)
+    .attr("y", 40)
+    .attr("class", "chartTitle")
+    .text("Percentage of protected natural areas in each country");
+
+};
+
+
+})();
+
+
 
 
