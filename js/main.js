f57ec66b0724b2ea7f4569ef0938df40f7b729fb
Ethan Westerkamp <ewesterkamp@wisc.edu>
2025-04-08 23:24:34 -0400

    Lab2
    Lab 2 Submission

--- js/main.js
+++ js/main.js
@@ -2,9 +2,23 @@
 
 (function(){
 
-var attrArray = ["protect_area", "real_gdp", "uaa", "renew_nrg", "protect_area", "CO2"];
+var attrArray = [ "Real GDP","Utilized Agriculture","Renewable Energy","Protected Area","CO2"];
 var expressed = attrArray[0];
 //execute script when window is loaded
+
+var chartWidth = window.innerWidth * 0.425,
+    chartHeight = 600;
+    leftPadding = 25,
+    rightPadding = 2,
+    topBottomPadding = 1,
+    chartInnerWidth = chartWidth - leftPadding - rightPadding,
+    chartInnerHeight = chartHeight - topBottomPadding * 2,
+    translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
+
+//create a scale to size bars proportionally to frame and for axis
+var yScale = d3.scaleLinear()
+.range([chartHeight, 0])
+.domain([-.1, 25]);
 
 window.onload = setMap();
 
@@ -15,12 +29,13 @@ function setMap() {
     var width = window.innerWidth *0.5,
     height = 600;    
 
+
     var map = d3.select("body")
     .append("svg")
     .attr("class", "map")
     .attr("width", width)
     .attr("height", height);
-
+    
     var projection = d3.geoAlbers()
         .center([10, 46.2])
         .rotate([-6, -7])
@@ -31,6 +46,7 @@ function setMap() {
     var path = d3.geoPath()
         .projection(projection);
 
+    
     //use Promise.all to parallelize asynchronous data loading
     var promises = [];
 
@@ -70,9 +86,11 @@ function setMap() {
 
         setEnumerationUnits(eu_countries, map, path, colorScale);
         
+        
         setChart(csvData, colorScale);
         console.log(csvData);
         console.log(eu_countries);
+        createDropdown(csvData)
     }
     };
 
@@ -101,7 +119,60 @@ function makeColorScale(data){
 };
 
 
+function createDropdown(csvData){
+    //add select element
+    var dropdown = d3.select("body")
+        .append("select")
+        .attr("class", "dropdown")
+        .on("change", function(){
+            changeAttribute(this.value, csvData)
+        });
 
+    //OPTIONS BLOCKS FROM EXAMPLE 1.1 LINES 8-19
+    var titleOption = dropdown.append("option")
+        .attr("class", "titleOption")
+        .attr("disabled", "true")
+        .text("Select Attribute");
+
+    //add attribute name options
+    var attrOptions = dropdown.selectAll("attrOptions")
+        .data(attrArray)
+        .enter()
+        .append("option")
+        .attr("value", function(d){ return d })
+        .text(function(d){ return d });
+};
+
+//dropdown change event handler
+function changeAttribute(attribute, csvData){
+    //change the expressed attribute
+    expressed = attribute;
+
+    //recreate the color scale
+    var colorScale = makeColorScale(csvData);
+
+    //recolor enumeration units
+    var countries = d3.selectAll(".Abbrev")
+        .transition()
+        .duration(300)
+        .style("fill", function(d){            
+            var value = d.properties[expressed];            
+            if(value) {                
+                return colorScale(value);            
+            } else {                
+                return "#ccc";            
+            }    
+        });
+    //Sort, resize, and recolor bars
+    var bars = d3.selectAll(".bar")
+        //Sort bars
+        .sort(function(a, b){
+            return b[expressed] - a[expressed];
+        })
+       
+        
+    updateChart(bars, csvData.length, colorScale);
+};
 
 function setGraticulte(map, path) {
     var graticule = d3.geoGraticule()
@@ -146,28 +217,39 @@ function joinData(eu_countries, csvData) {
 };
 
 function setEnumerationUnits(eu_countries, map, path, colorScale){
-    var specific_countries = map.selectAll()
-            .data(eu_countries)
-            .enter()
-            .append("path")
-            .attr("class", function(d){
-                return "ADMIN " + d.properties.ADMIN
-            })
-            .attr("d", path)
-            .style("fill", function(d){
-                var value = d.properties[expressed];
-                if(value) {
-                return colorScale(d.properties[expressed]);
-                } else {
-                    return "#ccc";
-                }
-            })
+    var specific_countries = map.selectAll(".Abbrev ")
+        .data(eu_countries)
+        .enter()
+        .append("path")
+        .attr("class", function(d){
+            return "Abbrev " + d.properties.ADMIN
+        })
+        .attr("d", path)
+        .style("fill", function(d){
+            var value = d.properties[expressed];
+            if(value) {
+                return colorScale(value);
+            } 
+            else {
+                return "#ccc";
+            }
+            
+        })
+        .on("mouseover", function(event, d){
+            highlight(d.properties);
+        })
+        .on("mouseout", function(event, d){
+            dehighlight(d.properties);
+        })
+        .on("mousemove", moveLabel);
+    var desc = specific_countries.append("desc")
+        .text('{"stroke": "#000", "stroke-width": "1px"}');
+   
 };
 
 function setChart(csvData, colorScale){
     //chart frame dimensions
-    var chartWidth = window.innerWidth * 0.425,
-        chartHeight = 600;
+    
 
     //create a second svg element to hold the bar chart
     var chart = d3.select("body")
@@ -176,62 +258,146 @@ function setChart(csvData, colorScale){
         .attr("height", chartHeight)
         .attr("class", "chart");
 
-    var yScale = d3.scaleLinear()
-    .range([0, chartHeight])
-    .domain([0, 105]);
+    //create a rectangle for chart background fill
+    var chartBackground = chart.append("rect")
+        .attr("class", "chartBackground")
+        .attr("width", chartInnerWidth)
+        .attr("height", chartInnerHeight)
+        .attr("transform", translate);
 
-    var bars = chart.selectAll(".bars")
+    //create a scale to size bars proportionally to frame and for axis
+  
+
+    //set bars for each province
+    var bars = chart.selectAll(".bar")
         .data(csvData)
         .enter()
         .append("rect")
         .sort(function(a, b){
-            return a[expressed]-b[expressed]
+            return b[expressed]-a[expressed]
         })
         .attr("class", function(d){
-            return "bars " + d.renew_nrg;
+            return "bar " + d.ADMIN;
         })
-        .attr("width", chartWidth / csvData.length - 1)
-        .attr("x", function(d, i){
-            return i * (chartWidth / csvData.length);
+        .attr("width", chartInnerWidth / csvData.length - 1)
+        .on("mouseover", function(event, d){
+            highlight(d);
         })
-        .attr("height", function(d){
-            return yScale(parseFloat(d[expressed]));
+        .on("mouseout", function(event, d){
+            dehighlight(d);
         })
-        .attr("y", function(d){
+        .on("mousemove", moveLabel);
+    var desc = bars.append("desc")
+        .text('{"stroke": "#444", "stroke-width": "1px"}');
+
+    //create a text element for the chart title
+    var chartTitle = chart.append("text")
+        .attr("x", 40)
+        .attr("y", 40)
+        .attr("class", "chartTitle")
+        .text("Percent of " + expressed + " in each country");
+
+    //create vertical axis generator
+    var yAxis = d3.axisLeft()
+        .scale(yScale);
+
+    //place axis
+    var axis = chart.append("g")
+        .attr("class", "axis")
+        .attr("transform", translate)
+        .call(yAxis);
+
+    //create frame for chart border
+    var chartFrame = chart.append("rect")
+        .attr("class", "chartFrame")
+        .attr("width", chartInnerWidth)
+        .attr("height", chartInnerHeight)
+        .attr("transform", translate);
+
+    updateChart(bars, csvData.length, colorScale);
+};
+
+function updateChart(bars, n, colorScale){
+    //position bars
+    bars.attr("x", function(d, i){
+            return i * (chartInnerWidth / n) + leftPadding;
+        })
+        //size/resize bars
+        .attr("height", function(d, i){
             return chartHeight - yScale(parseFloat(d[expressed]));
         })
-        .style("fill", function(d){
-            return colorScale(d[expressed]);
+        .attr("y", function(d, i){
+            return yScale(parseFloat(d[expressed])) + topBottomPadding;
+        })
+        //color/recolor bars
+        .style("fill", function(d){            
+            var value = d[expressed];            
+            if(value) {                
+                return colorScale(value);            
+            } else {                
+                return "#ccc";            
+            }    
+        });
+        var chartTitle = d3.select(".chartTitle")
+            .text("Percentage of " + expressed + " in each country");
+};
+
+function highlight(props){
+    //change stroke
+    var selected = d3.selectAll("." + props.ADMIN)
+        .style("stroke", "blue")
+        .style("stroke-width", "2");
+    setLabel(props)
+};
+
+function dehighlight(props){
+    var selected = d3.selectAll("." + props.ADMIN)
+        .style("stroke", function(){
+            return getStyle(this, "stroke")
+        })
+        .style("stroke-width", function(){
+            return getStyle(this, "stroke-width")
         });
 
-    var numbers = chart.selectAll(".numbers")
-    .data(csvData)
-    .enter()
-    .append("text")
-    .sort(function(a, b){
-        return a[expressed]-b[expressed]
-    })
-    .attr("class", function(d){
-        return "numbers " + d.renew_nrg;
-    })
-    .attr("text-anchor", "middle")
-    .attr("x", function(d, i){
-        var fraction = chartWidth / csvData.length;
-        return i * fraction + (fraction - 1) / 2;
-    })
-    .attr("y", function(d){
-        return chartHeight - yScale(parseFloat(d[expressed])) + 15;
-    })
-    .text(function(d){
-        return d[expressed];
-    });
+    function getStyle(element, styleName){
+        var styleText = d3.select(element)
+            .select("desc")
+            .text();
 
-    var chartTitle = chart.append("text")
-    .attr("x", 20)
-    .attr("y", 40)
-    .attr("class", "chartTitle")
-    .text("Percentage of protected natural areas in each country");
+        var styleObject = JSON.parse(styleText);
 
+        return styleObject[styleName];
+    };
+    d3.select(".infolabel")
+    .remove();
+};
+
+function setLabel(props){
+    //label content
+    var labelAttribute = "<h1>" + props[expressed] +
+        "</h1><b>" + expressed + "</b>";
+
+    //create info label div
+    var infolabel = d3.select("body")
+        .append("div")
+        .attr("class", "infolabel")
+        .attr("id", props.ADMIN + "_label")
+        .html(labelAttribute);
+
+    var regionName = infolabel.append("div")
+        .attr("class", "labelname")
+        .html(props.ADMIN);
+    
+};
+
+function moveLabel(){
+    //use coordinates of mousemove event to set label coordinates
+    var x = event.clientX + 10,
+        y = event.clientY - 75;
+
+    d3.select(".infolabel")
+        .style("left", x + "px")
+        .style("top", y + "px");
 };
 
 
